c063fdfea0a9a85895033fb239d841f75e6a8fa1
tuxp1 <tuxp1@zuk.com>
2017-04-28 18:13:54 +0800

    Finish SettingDefaultHandler

---
+++ src/com/tustar/concurrency/SettingDefaultHandler.java
@@ -0,0 +1,16 @@
+package com.tustar.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by tustar on 17-4-28.
+ */
+public class SettingDefaultHandler {
+
+    public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(new ExceptionThread2());
+    }
+}
