0c167a133e48246af6d2c0aab73c490c060e0dfe
tuxp1 <tuxp1@zuk.com>
2017-05-09 16:08:12 +0800

    Finish RestaurantWithQueues

---
+++ src/com/tustar/concurrency/restaurant2/Course.java
@@ -0,0 +1,21 @@
+package com.tustar.concurrency.restaurant2;
+
+/**
+ * Created by tustar on 17-5-9.
+ */
+public enum Course {
+    APPETIZER(Food.Appetizer.class),
+    MAINCOURSE(Food.MainCourse.class),
+    DESSERT(Food.Dessert.class),
+    COFFEE(Food.Coffee.class);
+
+    private Food[] valeus;
+
+    private Course(Class<? extends Food> kind) {
+        valeus = kind.getEnumConstants();
+    }
+
+    public Food randomSelection() {
+        return Enums.random(valeus);
+    }
+}
