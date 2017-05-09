94ce4a160ba3f0c0e7a8f5736fecebfb0bda2039
tuxp1 <tuxp1@zuk.com>
2017-05-09 16:53:49 +0800

    Finish CarBuilder

---
+++ src/com/tustar/concurrency/CarBuilder.java
@@ -0,0 +1,281 @@
+package com.tustar.concurrency;
+
+import javafx.scene.transform.Rotate;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * Created by tustar on 17-5-9.
+ */
+class Car {
+    private final int id;
+    private boolean engine = false, driveTrain = false, wheels = false;
+
+    public Car(int idn) {
+        id = idn;
+    }
+
+    public Car() {
+        this(-1);
+    }
+
+    public synchronized int getId() {
+        return id;
+    }
+
+    public synchronized void addEngine() {
+        engine = true;
+    }
+
+    public synchronized void addDriveTrain() {
+        driveTrain = true;
+    }
+
+    public synchronized void addWheels() {
+        wheels = true;
+    }
+
+    @Override
+    public synchronized String toString() {
+        return "Car " + id + " [" + " engine: " + engine
+                + " driveTrain: " + driveTrain
+                + " wheels: " + wheels + " ]";
+    }
+}
+
+class CarQueue extends LinkedBlockingQueue<Car> {
+}
+
+class ChassisBuilder implements Runnable {
+
+    private CarQueue carQueue;
+    private int counter = 0;
+
+    public ChassisBuilder(CarQueue carQueue) {
+        this.carQueue = carQueue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(500);
+                Car car = new Car(counter++);
+                System.out.println("ChassisBuilder created " + car);
+                carQueue.put(car);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted: ChassisBuilder");
+        }
+        System.out.println("ChassisBuilder off");
+    }
+}
+
+class Assembler implements Runnable {
+
+    private CarQueue chassisQueue, finshingQueue;
+    private Car car;
+    private CyclicBarrier barrier = new CyclicBarrier(4);
+    private RobotPool robotPool;
+
+    public Assembler(CarQueue chassisQueue, CarQueue finshingQueue, RobotPool robotPool) {
+        this.chassisQueue = chassisQueue;
+        this.finshingQueue = finshingQueue;
+        this.robotPool = robotPool;
+    }
+
+    public Car car() {
+        return car;
+    }
+
+    public CyclicBarrier barrier() {
+        return barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                car = chassisQueue.take();
+                // Hire robots to perform work:
+                robotPool.hire(EngineRobot.class, this);
+                robotPool.hire(DriveTrainRobot.class, this);
+                robotPool.hire(WheelRobot.class, this);
+                barrier.await(); // Until the robots finish
+                // Put car into finishingQueue or further work
+                finshingQueue.put(car);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Exiting Assembler via interrupt");
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Assembler off");
+    }
+}
+
+class Reporter implements Runnable {
+
+    private CarQueue carQueue;
+
+    public Reporter(CarQueue carQueue) {
+        this.carQueue = carQueue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println(carQueue.take());
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Exiting Reporter via interrupt");
+        }
+        System.out.println("Reporter off");
+    }
+}
+
+abstract class Robot implements Runnable {
+
+    private RobotPool pool;
+
+    public Robot(RobotPool pool) {
+        this.pool = pool;
+    }
+
+    protected Assembler assembler;
+
+    public Robot assignAssembler(Assembler assembler) {
+        this.assembler = assembler;
+        return this;
+    }
+
+    private boolean engage = false;
+
+    public synchronized void engage() {
+        engage = true;
+        notifyAll();
+    }
+
+    // The part of run() that's different for each robot:
+    abstract protected void performService();
+
+    @Override
+    public void run() {
+        try {
+            powerDown(); // Wait until needed
+            while (!Thread.interrupted()) {
+                performService();
+                assembler.barrier().await(); // Synchronize
+                // We're done with that job...
+                powerDown();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Exiting " + this + " via interrupt");
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(this + " off");
+    }
+
+    private synchronized void powerDown() throws InterruptedException {
+        engage = false;
+        assembler = null;
+        pool.release(this);
+        while (engage == false) {
+            wait();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName();
+    }
+}
+
+class EngineRobot extends Robot {
+
+    public EngineRobot(RobotPool pool) {
+        super(pool);
+    }
+
+    @Override
+    protected void performService() {
+        System.out.println(this + " installing engine");
+        assembler.car().addEngine();
+    }
+}
+
+class DriveTrainRobot extends Robot {
+
+    public DriveTrainRobot(RobotPool pool) {
+        super(pool);
+    }
+
+    @Override
+    protected void performService() {
+        System.out.println(this + " installing DriveTrain");
+        assembler.car().addDriveTrain();
+    }
+}
+
+class WheelRobot extends Robot {
+
+    public WheelRobot(RobotPool pool) {
+        super(pool);
+    }
+
+    @Override
+    protected void performService() {
+        System.out.println(this + " installing Wheels");
+        assembler.car().addWheels();
+    }
+
+}
+
+class RobotPool {
+
+    private Set<Robot> robots = new HashSet<>();
+
+    public synchronized void add(Robot robot) {
+        robots.add(robot);
+        notifyAll();
+    }
+
+    public synchronized void hire(Class<? extends Robot> robotType, Assembler assembler) throws InterruptedException {
+        for (Robot robot : robots) {
+            if (robot.getClass().equals(robotType)) {
+                robots.remove(robot);
+                robot.assignAssembler(assembler);
+                robot.engage();
+                return;
+            }
+            wait();
+            hire(robotType, assembler);
+        }
+    }
+
+    public synchronized void release(Robot robot) {
+        add(robot);
+    }
+}
+
+public class CarBuilder {
+    public static void main(String[] args) throws Exception {
+        CarQueue chassisQueue = new CarQueue();
+        CarQueue finishingQueue = new CarQueue();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        RobotPool robotPool = new RobotPool();
+        exec.execute(new EngineRobot(robotPool));
+        exec.execute(new DriveTrainRobot(robotPool));
+        exec.execute(new WheelRobot(robotPool));
+        exec.execute(new Assembler(chassisQueue, finishingQueue, robotPool));
+        exec.execute(new Reporter(finishingQueue));
+        exec.execute(new ChassisBuilder(chassisQueue));
+        TimeUnit.SECONDS.sleep(7);
+        exec.shutdownNow();
+    }
+}
